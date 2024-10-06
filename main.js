6077e973b69a137221f7d4ac38e3d66b83e0be4b
LiamKearn <contact@liamkearn.me>
2024-10-06 23:09:22 +1100

    Rename fullscreening function

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const updateWindow = (window) =>
-    browser.windows.update(window.id, {state: "fullscreen"});
+const fullscreenWindow = (window) => browser.windows.update(window.id, {state: "fullscreen"});
 
 browser.windows.getAll().then(
-    windowInfoArray => windowInfoArray.forEach(updateWindow),
+    windowInfoArray => windowInfoArray.forEach(fullscreenWindow),
     console.error
 );
 
-browser.windows.onCreated.addListener(window => setTimeout(updateWindow.bind(null, window), 50));
+browser.windows.onCreated.addListener(window =>
+    setTimeout(fullscreenWindow.bind(null, window), 50));
