4426b2f4bb7496b0d82945b6a8faa0e976bc27d5
LiamKearn <contact@liamkearn.me>
2024-10-06 23:10:05 +1100

    Add a example web-ext run script
    For my setup I want to see this working with specific preferences and
    for FF devedition.

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+web-ext run --devtools --firefox=firefoxdeveloperedition --pref=sidebar.revamp=true --pref=full-screen-api.ignore-widgets=true --pref=sidebar.verticalTabs=true --pref=browser.fullscreen.autohide=true
+
